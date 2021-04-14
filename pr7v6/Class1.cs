2397fc2060cfdbf7dd863a066e6a6a2e1796dabd
kosovskiy989 <kosovskiy989@gmail.com>
2021-04-14 13:51:49 +0300

    Добавьте файлы проекта.

---
+++ pr7v6/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace pr7v6
+{
+    public class Class1
+    {
+
+        public string modelCPU;
+        public string country;
+        public int kernel;
+        public string VidCard;
+        public string typeMemor;
+        public int potokiv;
+        public void CPUinfo()
+        {
+
+
+            Console.WriteLine(" Модель процесора : {0}\n країна виробник : {1}\n кількість ядер : {2}\n інтегрована відеопамять : {3}\n тип памяті : {4}\n кількість потоків : {5}", modelCPU, country, kernel, VidCard, typeMemor, potokiv);
+        }
+    }
+}
